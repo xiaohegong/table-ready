6cafb4de0f2cd8b0cd6a0dd67451b88a986f8261
Yujie Miao <yujie.miao@mail.utoronto.ca>
2019-10-31 23:37:35 -0400

    created menu component

--- react-app/src/Components/Restaurateur/Menu.jsx
+++ react-app/src/Components/Restaurateur/Menu.jsx
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import MenuItem from "./MenuItem";
+import RestaurantListItem from "./RestaurantListItem";
 
 class Menu extends Component {
     state = {  };
+    addRow(){
+        return <tr><th>
+            <MenuItem name="Burger"
+                     image={
+                         process.env.PUBLIC_URL +
+                         "/images/menu/burger.jpg"
+                     }/>
+        </th><th>
+            <MenuItem name="Burger"
+                      image={
+                          process.env.PUBLIC_URL +
+                          "/images/menu/burger.jpg"
+                      }/>
+        </th><th>
+            <MenuItem name="Burger"
+                      image={
+                          process.env.PUBLIC_URL +
+                          "/images/menu/burger.jpg"
+                      }/>
+        </th></tr>
+    }
     render() {
-        return ( <h1>items</h1> );
+        return ( <><h2 style={{display:'inline'}}>Your Restaurants</h2>
+            <button className={"addNewButton"}>Add New Item</button>
+        <table>{this.addRow()}{this.addRow()}{this.addRow()}</table></>);
     }
 }
 
