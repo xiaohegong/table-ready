db258da13361d64989e95c4884cb4263089e19af
Lantao <clt2738461982@gmail.com>
2019-11-07 15:13:04 -0500

    drag implemented

--- react-app/src/Components/Employee/employee.jsx
+++ react-app/src/Components/Employee/employee.jsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { withStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
 import './employee.css'
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
 import CardGroup from 'react-bootstrap/CardGroup'
@@ -19,6 +18,8 @@ import CheckIcon from '@material-ui/icons/Check';
 import '@y0c/react-datepicker/assets/styles/calendar.scss';
 import { DatePicker } from '@y0c/react-datepicker';
 import { scaleRotate	 as Menu } from 'react-burger-menu'
+import all_table from './dummy_table_data'
+import Draggable, {DraggableCore} from 'react-draggable';
 // fake data generator
 
 // a little function to help us with reordering the result
@@ -89,32 +90,56 @@ class employee extends Component {
       to_be_reserved:[],
       checkedG:false,
       current_date:null,
-      menu_open:false
+      menu_open:false,
+      current_table:null,
+      all_table:all_table.tables,
+      user_obj: null
     };
-    this.onDragEnd = this.onDragEnd.bind(this);
   }
 
-  idtoList = {
-    droppable: 'items',
-    droppable2: 'to_be_reserved'
-  };
 
   getList = (id) => this.state[this.idtoList[id]];
 
   handleChange = () => {
     this.setState({checkedG:!this.state.checkedG})
   };
+  handleDrag = (index) => {
+    const tmp = this.state.to_be_reserved[index]
+    this.setState({user_obj:tmp})
+  }
+  handleStop = () => {
+    this.setState({dragged:false})
+    
+  }
   change_menu_state = (index) => {
     this.setState({menu_open:!this.state.menu_open})
     this.state.to_be_reserved.push(this.state.items[index])
     this.setState({to_be_reserved:this.state.to_be_reserved})
-    console.log(this.state.to_be_reserved)
    }
   info = (e) => {
-    console.log("hi")
+    // console.log("hi")
   }
   removefocus = (e) => {
     e.preventDefault()
+  }
+  ondragstart = (index) => {
+
+  }
+  /* change color of card */ 
+  checkcapacity = (index) => {
+    const cur_table = document.getElementById(`Table-${index}`)
+    this.setState({current_table:cur_table})
+    const cur_table_obj = this.state.all_table[index]
+    console.log(this.state.user_obj)
+    if (cur_table_obj.table_capacity >= this.state.user_obj.people){
+      cur_table.style.backgroundColor = "green"
+    }
+    else{
+      cur_table.style.backgroudColor = "red"
+    }
+  }
+  resumecard = () => {
+    // console.log("left")
   }
   showdate = (value) => {
     const year = value.$y
@@ -126,43 +151,7 @@ class employee extends Component {
   handleStateChange = (state) => {
     this.setState({menu_open:state.isOpen})
   }
-  onDragEnd(result) {
-    // dropped outside the list
-    const { source, destination } = result;
-
-    // dropped outside the list
-    if (!destination) {
-        return;
-    }
-
-    if (source.droppableId === destination.droppableId) {
-        const items = reorder(
-            this.getList(source.droppableId),
-            source.index,
-            destination.index
-        );
-
-        let state = { items };
-
-        if (source.droppableId === 'droppable2') {
-            state = { selected: items };
-        }
-
-        this.setState(state);
-    } else {
-        const result = move(
-            this.getList(source.droppableId),
-            this.getList(destination.droppableId),
-            source,
-            destination
-        );
-
-        this.setState({
-            items: result.droppable,
-            selected: result.droppable2
-        });
-    }
-    }
+ 
   // Normally you would want to split things out into separate components.
   // But in this example everything is just done in one place for simplicity
   render() {
@@ -173,53 +162,51 @@ class employee extends Component {
           <span id = "reservation_container" onMouseDown = {this.removefocus}>
             {
               this.state.to_be_reserved.map((item,index) => (
-                <Card className = "usercard" bg="light" style={{ width: '18rem' }} onMouseDown = {this.startdragging}>
-                  <Card.Header className = "header-of-card">
-                    <div className = "pic-container">
-                      <strong>
-                        {item.Name}
-                      </strong>
-                      <img className = "user-pic"src = "./images/restaurant_images/boy.png"></img>
-                    </div>
-                  </Card.Header>
-                  <Card.Body>
-                    <div>
-                      <span><img className = "info-png" src = "./images/restaurant_images/calendar.png"></img><span className = 
-                      "reservation_time">{item.estimated_time}</span><span className = "reservation_date">/{item.date_of_arrival}</span></span>
-                    </div>
-                    <div className = "num_people">
-                      <span><img className = "info-png" src = "./images/restaurant_images/avatar.png"></img><span className = "attendence">{item.people}</span></span>
-                    </div>
-                    <div className = "user_profile_holder">
-                      <div className = "check-container">  
-                          <button class="accept-button" onClick = {(e) => this.change_menu_state(index)} onMouseDown = {this.removefocus}><img src = "./images/restaurant_images/done-tick.png"></img></button>
-                          <button class="reject-button" onClick = {(e) => this.change_menu_state(index)} onMouseDown = {this.removefocus}><img src = "./images/restaurant_images/no-stopping.png"></img></button>
+                <Draggable onStart = {() => this.ondragstart(index)} onDrag={() => this.handleDrag(index)}  onStop={this.handleStop}>
+                  <Card id = {`usercard-${index}`} draggable = "true" style={{backgroundColor:"#f8f9fa", width: '18rem' }}>
+                    <Card.Header className = "header-of-card">
+                      <div className = "pic-container">
+                        <strong>
+                          {item.Name}
+                        </strong>
+                        <img className = "user-pic"src = "./images/restaurant_images/boy.png"></img>
                       </div>
-                    </div>
-                  </Card.Body>
-                </Card>
+                    </Card.Header>
+                    <Card.Body>
+                      <div>
+                        <span><img className = "info-png" src = "./images/restaurant_images/calendar.png"></img><span className = 
+                        "reservation_time">{item.estimated_time}</span><span className = "reservation_date">/{item.date_of_arrival}</span></span>
+                      </div>
+                      <div className = "num_people">
+                        <span><img className = "info-png" src = "./images/restaurant_images/avatar.png"></img><span className = "attendence">{item.people}</span></span>
+                      </div>
+                      <div className = "user_profile_holder">
+                        <div className = "check-container">  
+                            <button class="accept-button" onClick = {(e) => this.change_menu_state(index)} onMouseDown = {this.removefocus}><img src = "./images/restaurant_images/done-tick.png"></img></button>
+                            <button class="reject-button" onClick = {(e) => this.change_menu_state(index)} onMouseDown = {this.removefocus}><img src = "./images/restaurant_images/no-stopping.png"></img></button>
+                        </div>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Draggable>
               ))
             }
           </span>
           <span id = "avaliable_seats_container" onMouseDown = {this.removefocus}>
             {
-              this.state.items.map((item,index) => (
-                <Card className = "usercard" bg="light" style={{ width: '18rem' }}>
+              this.state.all_table.map((item,index) => (
+                <Card id = {`Table-${index}`} className = "tablecard" style={{backgroundColor:"#f8f9fa",  width: '18rem' }}  onMouseEnter = {(e) => this.checkcapacity(index)} onMouseLeave = {this.resumecard}>
                   <Card.Header className = "header-of-card">
                     <div className = "pic-container">
                       <strong>
-                        {item.Name}
+                        {`Table-${index+1}`}
                       </strong>
-                      <img className = "user-pic"src = "./images/restaurant_images/boy.png"></img>
+                      <img className = "user-pic"src = "./images/restaurant_images/table.png"></img>
                     </div>
                   </Card.Header>
                   <Card.Body>
                     <div>
-                      <span><img className = "info-png" src = "./images/restaurant_images/calendar.png"></img><span className = 
-                      "reservation_time">{item.estimated_time}</span><span className = "reservation_date">/{item.date_of_arrival}</span></span>
-                    </div>
-                    <div className = "num_people">
-                      <span><img className = "info-png" src = "./images/restaurant_images/avatar.png"></img><span className = "attendence">{item.people}</span></span>
+                      <span>Capacity: {item.table_capacity}</span>
                     </div>
                     <div className = "user_profile_holder">
                       <div className = "check-container">  
