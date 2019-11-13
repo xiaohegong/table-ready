fdfc192912e8db4b815a0fd6683bd0ea1937bb1a
Huakun Shen <shenhuakun@outlook.com>
2019-11-12 20:21:00 -0500

    connected to mongodb atlas

--- react-app/src/mongoose.js
+++ react-app/src/mongoose.js
@@ -1,22 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const url = "mongodb://trtest1tor.documents.azure.com:10255/?ssl=true";
-
+const url =
+  "mongodb+srv://admin:admin@cluster0-hopsi.mongodb.net/table_ready?retryWrites=true&w=majority";
 function connect() {
-    mongoose.set('debug', true);
-    return mongoose.connect(url, {
-        auth: {
-            user: 'trtest1tor',
-            password: 'XeNetQxjzUPT8ine4z0VNCjD6R5S4WJwii8cXuRJ0z7aBqbd46An3Ndf7FX8Js1x9tAyihpL5X8tFbtpy18qHw==',
-        }
-    }, function (err, db) {
-        if (!err) {
-            console.log("Successfully connected to database");
-        }
-    });
+  mongoose.set("debug", true);
+
+  return mongoose
+    .connect(url, { useNewUrlParser: true })
+    .catch(error => console.log(error));
+  //   return mongoose.connect(
+  //     url,
+  //     {
+  //       auth: {
+  //         user: "trtest1tor",
+  //         password:
+  //           "XeNetQxjzUPT8ine4z0VNCjD6R5S4WJwii8cXuRJ0z7aBqbd46An3Ndf7FX8Js1x9tAyihpL5X8tFbtpy18qHw=="
+  //       }
+  //     },
+  //     function(err, db) {
+  //       if (!err) {
+  //         console.log("Successfully connected to database");
+  //       }
+  //     }
+  //   );
 }
 
 module.exports = {
-    connect,
-    mongoose
+  connect,
+  mongoose
 };
