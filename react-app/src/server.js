2e984ab06c913a6d32d21e2af4ae60d054599bd6
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-11-13 22:10:19 -0500

    finish update user info in admin setting page

--- react-app/src/server.js
+++ react-app/src/server.js
@@ -5,7 +5,7 @@ const log = console.log;
 const express = require("express");
 const bodyParser = require("body-parser"); // middleware for parsing HTTP body
 const app = express();
-const { ObjectID } = require("mongodb");
+const {ObjectID} = require("mongodb");
 const User = require("./models/user.js");
 const Restaurant = require("./models/Restaurant.js");
 const MenuItem = require("./models/MenuItem.js");
@@ -13,7 +13,7 @@ const Waitlist = require("./models/waitlist.js")
 
 /* Use statements for the server */
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 require("./mongoose").connect();
 
@@ -23,9 +23,9 @@ app.get("/", (req, res) => {
 
 app.get("/api/customers", (req, res) => {
   const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" }
+    {id: 1, firstName: "John", lastName: "Doe"},
+    {id: 2, firstName: "Brad", lastName: "Traversy"},
+    {id: 3, firstName: "Mary", lastName: "Swanson"}
   ];
 
   res.json(customers);
@@ -68,7 +68,7 @@ app.post("/restaurant/newRestaurant", (req, res) => {
     })
     .catch(err => {
       log(err);
-      res.send({ code: 404, err });
+      res.send({code: 404, err});
     });
 });
 
@@ -85,7 +85,7 @@ app.post("/restaurant/updateRestaurant", (req, res) => {
     })
     .catch(err => {
       log(err);
-      res.send({ code: 404, err });
+      res.send({code: 404, err});
     });
 });
 
@@ -104,7 +104,7 @@ app.post("/restaurant/newMenuItem", (req, res) => {
     })
     .catch(err => {
       log(err);
-      res.send({ code: 404, err });
+      res.send({code: 404, err});
     });
   // return new Promise((resolve, reject) => {
   //
@@ -112,13 +112,13 @@ app.post("/restaurant/newMenuItem", (req, res) => {
 });
 
 app.post("/restaurant/findRestaurantByOwner", (req, res) => {
-  Restaurant.find({ owner: req.body.owner }).then(
+  Restaurant.find({owner: req.body.owner}).then(
     restaurant => {
       console.log(restaurant);
       res.send(restaurant);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
   // return new Promise((resolve, reject) => {
@@ -129,12 +129,12 @@ app.post("/restaurant/findRestaurantByOwner", (req, res) => {
 app.post("/restaurant/findEmployeesByRestaurant", (req, res) => {
   const restaurant_id = req.body.restaurant_id;
   console.log("restaurant_id:   --- ", restaurant_id);
-  User.find({ workFor: restaurant_id }).then(
+  User.find({workFor: restaurant_id}).then(
     users => {
       res.send(users);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
 
@@ -151,7 +151,7 @@ app.post("/restaurant/updateDressCode", (req, res) => {
       res.send(user);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
 });
@@ -164,7 +164,7 @@ app.post("/restaurant/updateReservation", (req, res) => {
       res.send(user);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
 });
@@ -176,9 +176,10 @@ app.post("/restaurant/add_employee", (req, res) => {
   console.log("username:", username);
   if (username && restaurant_id) {
     User.findOneAndUpdate(
-      { username: username },
-      { workFor: restaurant_id }
-    ).then(user => {});
+      {username: username},
+      {workFor: restaurant_id}
+    ).then(user => {
+    });
   }
 });
 
@@ -187,7 +188,7 @@ app.post("/restaurant/delete_employee", (req, res) => {
   const user_id = req.body.user_id;
   console.log("\n\n\n/restaurant/delete_employee\n\n\n");
   if (user_id) {
-    User.findByIdAndUpdate(user_id, { workFor: "" }, (err, user) => {
+    User.findByIdAndUpdate(user_id, {workFor: ""}, (err, user) => {
       if (err) {
         console.log(err);
         res.send(err);
@@ -200,12 +201,12 @@ app.post("/restaurant/delete_employee", (req, res) => {
 });
 
 app.post("/restaurant/findRestaurant", (req, res) => {
-  Restaurant.find({ _id: req.body._id }).then(
+  Restaurant.find({_id: req.body._id}).then(
     user => {
       res.send(user);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
   // return new Promise((resolve, reject) => {
@@ -228,7 +229,7 @@ app.post("/waitlist/newWaitlist", (req, res) => {
     })
     .catch(err => {
       log(err);
-      res.send({ code: 404, err});
+      res.send({code: 404, err});
     });
 });
 
@@ -238,7 +239,7 @@ app.post("/waitlist/getWaitlist", (req, res) => {
       res.send(waitlist);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
   // return new Promise((resolve, reject) => {
@@ -247,12 +248,12 @@ app.post("/waitlist/getWaitlist", (req, res) => {
 });
 
 app.post("/waitlist/getWaitlistById", (req, res) => {
-  Waitlist.find({ _id: req.body._id }).then(
+  Waitlist.find({_id: req.body._id}).then(
     waitlist => {
       res.send(waitlist);
     },
     error => {
-      res.send({ code: 404, error });
+      res.send({code: 404, error});
     }
   );
   // return new Promise((resolve, reject) => {
@@ -261,7 +262,7 @@ app.post("/waitlist/getWaitlistById", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-  User.find({}, function(err, users) {
+  User.find({}, function (err, users) {
     if (err) {
       log(err);
       return err;
@@ -272,7 +273,7 @@ app.get("/api/users", (req, res) => {
 });
 
 app.get("/api/restaurants", (req, res) => {
-  Restaurant.find({}, function(err, restaurants) {
+  Restaurant.find({}, function (err, restaurants) {
     if (err) {
       log(err);
       return err;
@@ -328,7 +329,7 @@ app.put("/user/:id", (req, res) => {
   User.findByIdAndUpdate(
     req.params.id,
     req.body,
-    { new: true },
+    {new: true},
     (err, todo) => {
       if (err) {
         return res.status(500).send(err);
@@ -341,14 +342,14 @@ app.put("/user/:id", (req, res) => {
 });
 
 app.put("/updateWaitlist/:id", (req, res) => {
-  Waitlist.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  Waitlist.findByIdAndUpdate(req.params.id, req.body, {new: true})
     .then(() => {
       res.send("Waitlist " + id + " updated");
     })
     .catch(err => {
       res.status(400).json('Error: ' + err);
-    }); 
-})
+    });
+});
 
 app.put("/updateRestWaitlist/:id", (req, res) => {
   Restaurant.findByIdAndUpdate(req.params.id, req.body, {new: true})
@@ -357,8 +358,21 @@ app.put("/updateRestWaitlist/:id", (req, res) => {
     })
     .catch(err => {
       res.status(400).json('Error: ' + err);
-    }); 
-})
+    });
+});
+
+app.get("/user/:id", (req, res) => {
+  log("The server side got id is: ");
+  log(req.params.id);
+  User.findById(req.params.id)
+    .then(user => {
+      console.log("this is from server side");
+      log(user);
+      res.json(user);
+    })
+    .catch(error => res.status(400).json("Err " + error));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   log("Listening on port 5000...");
